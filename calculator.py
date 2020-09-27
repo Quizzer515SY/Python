85c4ea8c75846666caaa5ee37cb85f4faa823872
Quizzer515SY <62062921+Quizzer515SY@users.noreply.github.com>
2020-09-27 12:15:36 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,46 @@
 def main():
-	true = True
-	while true:
+	
+	num1 = 0
+	num2 = 0
+	
+	question = True
+	while question:
 		calc = input("Would you like to add, sub, mul, or div?\n")
 		list_of_calcs = ["add","sub","mul","div"]
 		
 		for i in list_of_calcs:
 			if(i == calc):
-				true = False
-		if true:
-			print("That is not a valid option.\n")
-			
-	num1 = float(input("First number "))
-	num2 = float(input("Second number "))
-	
-	def cal(operation,number1,number2):
-		if operation == "add":
-			return number1+number2
-		if operation == "sub":
-			return number1-number2
-		if operation == "mul":
-			return number1*number2
-		if operation == "div":
-			return number1/number2
+				question = False
 		
-	result = cal(calc,num1,num2)
+		if question:
+			print("That is not a valid option.\n")
+	
+
+	while True:
+		try:
+			num1 = float(input("First number "))
+			break
+		except ValueError:
+			print("Oops! That is not a valid number")
+
+	while True:
+		try:
+			num2 = float(input("Second number "))
+			break
+		except ValueError:
+			print("Oops! That is not a valid number")
+
+	def cal(num1, num2, operation):
+		if operation == "add":
+			return num1 + num2
+		if operation == "sub":
+			return num1 - num2
+		if operation == "mul":
+			return num1 * num2
+		if operation == "div":
+			return num1 / num2
+		
+	result = cal(num1,num2,calc)
 	print(result)
 
 main()
